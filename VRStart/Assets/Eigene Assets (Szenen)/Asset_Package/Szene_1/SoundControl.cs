57835da914aa7e13e4889a444706c57908cc5cfb
unknown <marvin.busch@live.de>
2018-01-16 11:55:16 +0100

    Licht in El Dorado

---
+++ VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/Szene_1/SoundControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundControl : MonoBehaviour {
+
+	public GameObject Lampe;
+	
+	// Update is called once per frame
+	void Update () {
+		if(Lampe.)
+	}
+}
