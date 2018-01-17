a625e9b5f565ead2646ef81afda4e629e72b9a7c
Marvin Busch <marvin.busch@live.de>
2018-01-17 11:02:47 +0100

    Matrix Hände

--- VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/Szene_2-Matrix/MatrixLightControl.cs
+++ VRStart/Assets/Eigene Assets (Szenen)/Asset_Package/Szene_2-Matrix/MatrixLightControl.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class MatrixLightControl : MonoBehaviour {
 
-public float StartTime = 5.0f;
-public float Duration = 10.0f;
-protected float MyTime = 0f;
+	public float StartTime = 5.0f;
+	public float Duration = 10.0f;
+	protected float MyTime = 0f;
 
-public Transform[] Lights;
-public float LightIntensity = 1f;
+	public Transform[] Lights;
+	public float LightIntensity = 1f;
 
-public Transform Türe;
-public float LightIntensityTüre = 1f;
+	public Transform Türe;
+	public float LightIntensityTüre = 1f;
 
-public Transform Raum;
-public GameObject Hände;
-public ParticleSystem MatrixRegen;
+	public Transform Raum;
+	public GameObject Hände;
+	public ParticleSystem MatrixRegen;
+	public GameObject Hand_D;
+	public GameObject Hand_U;
+	public GameObject Pille_D;
+	public GameObject Pille_U;
 
-private bool Go = false;
-private bool end = false;
+
+	private bool Go = false;
+	private bool end = false;
 
 	// Use this for initialization
 	void Start () {
@@ -31,7 +36,11 @@ private bool end = false;
 		}	
 		Türe.GetComponent<Light>().intensity = 0f;
 		Hände.SetActive(false);
-		Raum.transform.localScale += new Vector3(0, 0, 5);
+		/*Hand_U.GetComponent<Renderer>().material.color = new Color (Hand_U.GetComponent<Renderer>().material.color.r, Hand_U.GetComponent<Renderer>().material.color.g, Hand_U.GetComponent<Renderer>().material.color.b, 0);
+		Hand_D.GetComponent<Renderer> ().material.color = new Color (Hand_D.GetComponent<Renderer>().material.color.r, Hand_D.GetComponent<Renderer>().material.color.g, Hand_D.GetComponent<Renderer>().material.color.b, 0);
+		Pille_U.GetComponent<Renderer>().material.color = new Color (Pille_U.GetComponent<Renderer>().material.color.r, Pille_U.GetComponent<Renderer>().material.color.g, Pille_U.GetComponent<Renderer>().material.color.b, 0);
+		Pille_D.GetComponent<Renderer> ().material.color = new Color (Pille_D.GetComponent<Renderer>().material.color.r, Pille_D.GetComponent<Renderer>().material.color.g, Pille_D.GetComponent<Renderer>().material.color.b, 0);
+		*/Raum.transform.localScale += new Vector3(0, 0, 5);
 	}
 	
 	// Update is called once per frame
@@ -53,7 +62,11 @@ private bool end = false;
 					Lights[i].GetComponent<Light>().intensity = LightIntensity * (MyTime/Duration);
 				}
 				Türe.GetComponent<Light>().intensity = LightIntensityTüre * (MyTime/Duration);
-			}
+				/*Hand_U.GetComponent<Renderer>().material.color = new Color (Hand_U.GetComponent<Renderer>().material.color.r, Hand_U.GetComponent<Renderer>().material.color.g, Hand_U.GetComponent<Renderer>().material.color.b, 1 * (MyTime/Duration));
+				Hand_D.GetComponent<Renderer> ().material.color = new Color (Hand_D.GetComponent<Renderer>().material.color.r, Hand_D.GetComponent<Renderer>().material.color.g, Hand_D.GetComponent<Renderer>().material.color.b, 1 * (MyTime/Duration));
+				Pille_U.GetComponent<Renderer>().material.color = new Color (Pille_U.GetComponent<Renderer>().material.color.r, Pille_U.GetComponent<Renderer>().material.color.g, Pille_U.GetComponent<Renderer>().material.color.b, 0);
+				Pille_D.GetComponent<Renderer> ().material.color = new Color (Pille_D.GetComponent<Renderer>().material.color.r, Pille_D.GetComponent<Renderer>().material.color.g, Pille_D.GetComponent<Renderer>().material.color.b, 0);
+			*/}
 		}
 		
 	}
